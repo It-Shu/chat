ad01345c4be5dc5d8b06b36d69fd3077fe4ce40f
It-Shu <it.shustov@gmail.com>
2021-11-21 23:25:38 +0300

    added webSocket connection

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import {ChatPage} from "./pages/ChatPage/ChatPage";
+// import {ChatPage} from "./pages/ChatPage/ChatPage";
+import {AddMessage} from "./pages/AddMassage/AddMessage";
 
 function App() {
   return (
     <div>
-        <ChatPage/>
+        <AddMessage/>
+        {/*<ChatPage/>*/}
     </div>
   );
 }
--- src/pages/AddMassage/AddMessage.tsx
+++ src/pages/AddMassage/AddMessage.tsx
@@ -1,16 +1,87 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 
 export const AddMessage = () => {
+
+    const [messages, setMessages] = useState<Array<any>>([]);
+    const [value, setValue] = useState('');
+    const socket = useRef<WebSocket>()
+    const [connected, setConnected] = useState(false);
+    const [username, setUsername] = useState('')
+
+    function connect() {
+        socket.current = new WebSocket('wss://ws.qexsystems.ru')
+
+        socket.current.onopen = () => {
+            setConnected(true)
+            const message = {
+                event: 'connection',
+                username,
+                id: Date.now()
+            }
+            socket.current?.send(JSON.stringify(message))
+        }
+        socket.current.onmessage = (e: MessageEvent) => {
+            const message = JSON.parse(e.data)
+            setMessages(prev => [message, ...prev])
+        }
+
+        socket.current.onclose = () => {
+            console.log('Socket закрыт')
+        }
+        socket.current.onerror = () => {
+            console.log('Socket произошла ошибка')
+        }
+    }
+
+    const sendMessage = async () => {
+
+        const message = {
+            username,
+            message: value,
+            id: Date.now(),
+            event: 'message'
+        }
+        socket.current?.send(JSON.stringify(message))
+        setValue('')
+    }
+    if (!connected) {
+        return (
+            <div>
+                <div>
+                    <input
+                        type="text"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        placeholder='Enter your Name'
+                    />
+                    <button onClick={connect}>Enter</button>
+                </div>
+            </div>
+        )
+    }
+
     return (
         <div>
             <div>
-                <textarea></textarea>
+                <div>
+                    <input
+                        value={value}
+                        onChange={(e) => setValue(e.target.value)}
+                        type="text"
+                    />
+                    <button onClick={sendMessage}>Send</button>
+                </div>
+                <div>
+                    {messages.map((mess) => (
+                        <div key={mess.id}>
+                            {mess.event === 'connection'
+                                ? <div>User {mess.username} is connected</div>
+                                : <div>{mess.username}: {mess.message}</div>
+                            }
+                        </div>
+                    ))}
+                </div>
             </div>
-            <div>
-                <button>Sent</button>
-            </div>
-
-
         </div>
     );
 };
